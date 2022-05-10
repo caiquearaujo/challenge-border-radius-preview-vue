c25b2b0a065154bfc5f05c33f7bbd0c22d47a1bf
Caique Araujo <caique@studiopiggly.com.br>
2022-05-10 11:59:15 -0300

    DimInput comp :: input a value to a corner

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,40 @@
 <template>
-	<custom-box
-		topLeft="10"
-		topRight="10"
-		bottomRight="10"
-		bottomLeft="10" />
+	<div class="flex-wrapper">
+		<div class="column">
+			<dim-input corner="top-left" v-model="corners.topLeft" />
+		</div>
+		<div class="column">
+			<dim-input corner="top-right" v-model="corners.topRight" />
+		</div>
+		<div class="column">
+			<dim-input corner="bottom-left" v-model="corners.bottomLeft" />
+		</div>
+		<div class="column">
+			<dim-input corner="bottom-right" v-model="corners.bottomRight" />
+		</div>
+	</div>
+	<custom-box v-bind="corners" />
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
 import CustomBox from './components/CustomBox.vue';
+import DimInput from './components/DimInput.vue';
 
 export default defineComponent({
 	name: 'App',
-	components: { CustomBox },
+
+	components: { CustomBox, DimInput },
+
+	data() {
+		return {
+			corners: {
+				topLeft: '10px',
+				topRight: '10px',
+				bottomRight: '10px',
+				bottomLeft: '10px',
+			},
+		};
+	},
 });
 </script>
---
+++ src/components/DimInput.vue
@@ -0,0 +1,45 @@
+<template>
+	<div class="dim-input">
+		<label :class="corner">{{ corner }}</label>
+		<input
+			:id="corner"
+			:name="corner"
+			type="text"
+			:required="true"
+			:value="modelValue"
+			@input="onChange" />
+	</div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from '@vue/runtime-core';
+
+export default defineComponent({
+	name: 'DimInput',
+
+	emits: ['update:modelValue', 'afterChange'],
+
+	props: {
+		corner: {
+			type: String,
+			required: true,
+		},
+
+		modelValue: {
+			type: String,
+			default: '0px',
+		},
+	},
+
+	methods: {
+		onChange(e: Event & { target: HTMLInputElement }): void {
+			this.$emit('update:modelValue', e.target.value);
+			this.$emit('afterChange', e.target.value);
+		},
+	},
+});
+</script>
+
+<style lang="scss">
+@import '@/styles/components/dim-input.scss';
+</style>
